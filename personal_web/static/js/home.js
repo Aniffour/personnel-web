d0d1f0aebc7773831a5a1a752948519c65cf2353
Aniffour <As5828801@gmail.com>
2023-08-07 17:36:13 +0100

    add some js to img in home

--- personal_web/static/js/home.js
+++ personal_web/static/js/home.js
@@ -1,16 +1,3 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const links = document.querySelectorAll('nav ul li a');
-  links.forEach(link => {
-    link.addEventListener('click', (event) => {
-      event.preventDefault();
-      const targetId = event.target.getAttribute('href');
-      document.querySelector(targetId).scrollIntoView({ behavior: 'smooth' });
-    });
-  });
-});
-
-
-
 let img = document.querySelector('.img')
 img.addEventListener('mouseover' , (el)=>el.currentTarget.style.width = '50%')
 img.addEventListener('mouseout' , (el)=>el.currentTarget.style.width = '25%')
