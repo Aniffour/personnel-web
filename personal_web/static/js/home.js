06de2e7b1a42bc7a96212cee446b39bbc0d5a1e7
Aniffour <As5828801@gmail.com>
2023-08-07 17:32:28 +0100

    add some js to img in home

--- personal_web/static/js/home.js
+++ personal_web/static/js/home.js
@@ -1,15 +1,16 @@
-// You can add JavaScript functionality here if needed
-// For example, smooth scrolling to section anchors on click
-
-// Example of smooth scrolling
 document.addEventListener('DOMContentLoaded', () => {
-    const links = document.querySelectorAll('nav ul li a');
-    links.forEach(link => {
-      link.addEventListener('click', (event) => {
-        event.preventDefault();
-        const targetId = event.target.getAttribute('href');
-        document.querySelector(targetId).scrollIntoView({ behavior: 'smooth' });
-      });
+  const links = document.querySelectorAll('nav ul li a');
+  links.forEach(link => {
+    link.addEventListener('click', (event) => {
+      event.preventDefault();
+      const targetId = event.target.getAttribute('href');
+      document.querySelector(targetId).scrollIntoView({ behavior: 'smooth' });
     });
   });
-  
+});
+
+
+
+let img = document.querySelector('.img')
+img.addEventListener('mouseover' , (el)=>el.currentTarget.style.width = '50%')
+img.addEventListener('mouseout' , (el)=>el.currentTarget.style.width = '25%')
